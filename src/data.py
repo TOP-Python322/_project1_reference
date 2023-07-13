834acdec5aa5c68a821b900c3f2bef3e0935258c
Gennadiy <renardes@gmail.com>
2023-07-13 22:09:57 +0500

    пути к файлам данных

--- src/data.py
+++ src/data.py
@@ -2,6 +2,15 @@
 Вспомогательный модуль: глобальные переменные и условные константы.
 """
 
+# стандартная библиотека
+from pathlib import Path
+from sys import path
+
+
+ROOT_DIR = Path(path[0]).parent
+PLAYERS_DB_PATH = ROOT_DIR / 'data/players.ini'
+SAVES_DB_PATH = ROOT_DIR / 'data/saves'
+
 
 COMMANDS = {
     'новая партия': ('new', 'игра'),
