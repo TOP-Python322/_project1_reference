0026409045460f1eec9973479ecbb99707c47a7d
Gennadiy <renardes@gmail.com>
2023-07-13 23:05:54 +0500

    добавление get_name()

--- src/players.py
+++ src/players.py
@@ -1,3 +1,24 @@
 """
 Основной модуль: взаимодействие с игроками.
 """
+
+# проект
+import data
+import utils
+
+
+def get_name() -> None:
+    """"""
+    while True:
+        name = input(data.MESSAGES['ввод имени'])
+        if data.name_pattern.fullmatch(name):
+            break
+        print(data.MESSAGES['некорректное имя'])
+    if name not in data.players_db:
+        data.players_db[name] = {
+            'wins': 0,
+            'fails': 0,
+            'ties': 0
+        }
+    utils.write_players()
+
