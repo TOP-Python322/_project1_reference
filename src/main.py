8190d6f540f426090e79272e5a0efdce4c648d9e
Gennadiy <renardes@gmail.com>
2023-07-13 21:47:15 +0500

    основной управляющий контур

--- src/main.py
+++ src/main.py
@@ -1,3 +1,37 @@
 """
 Главный модуль: точка входа.
 """
+
+# проект
+import data
+import game
+import help
+import players
+import utils
+
+# 1. Чтение файлов данных
+# 2. ЕСЛИ первый запуск:
+if utils.read_players():
+    # вывод раздела помощи 
+    help.show_help()
+
+# 3. Запрос имени игрока
+players.get_player()
+
+# суперцикл (главное меню)
+while True:
+    # 4. Ожидание ввода команды игрока
+    command = input(data.PROMPT)
+    
+    if command in data.COMMANDS['новая партия']:
+        game.mode()
+        result = game.game()
+        if result:
+            players.update()
+    
+    # elif command in data.COMMANDS['']:
+    
+    elif command in data.COMMANDS['выход']:
+        break
+
+# 20. Действия перед завершением работы приложения
